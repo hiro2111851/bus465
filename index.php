18f8d3d41ee621f736f6addfb3806040bda2ad3e
Hiro Nomura <37918204+hiro2111851@users.noreply.github.com>
2020-10-13 14:42:23 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- 
+    Page Name: Home Page
+
+    Page Description: 
+
+        Home page for our project
+
+    Created By: Hiro
+-->
+
+<!DOCTYPE html>
+
+<html lang=en>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+
+</html>
