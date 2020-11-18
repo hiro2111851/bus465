aa7bd59f03840b6676164919b32e454530311089
Hiro Nomura <37918204+hiro2111851@users.noreply.github.com>
2020-11-17 20:01:50 -0800

    Update orderconfirmation.php

--- orderconfirmation.php
+++ orderconfirmation.php
@@ -183,30 +183,59 @@ Thank you for your order!
     </h1><hr><br>
 <p>Order #<?php echo $order;?></p> 
 
-<p class='alert alert-danger'>Please e-transfer $<?php echo $total;?> to cookielover@gmail.com with your order # above in your e-transfer message section.</p>
+<p class='alert alert-danger mb-3'>Please e-transfer $<?php echo $total;?> to cookielover@gmail.com with your order # above in your e-transfer message section.</p>
+
+<div id='order_summary' class="w-75 mx-auto">
+<h3 align=left>Order Summary: </h3>
+<?php
+foreach ($_SESSION['shopping_cart'] as $cart_item) {?>
+    <div class="row p-3">
+        <div class="col-4">
+            <img class="img-responsive p-1 w-75" src="<?php echo $cart_item['img_link'];?>">    
+        </div>
+        <div class="col-8">
+            <h3 align=left style="font-size: 24px;"><?php echo $cart_item['product_name'];?></h3>
+            <!-- Price -->
+            <div class="row">
+                <div class="col-4">
+                    <p>
+                        Price:
+                        <br>Quantity:
+                        <br>Batch:
+                    </p>
+                </div>
+                <div class="col-8">
+                    <p style="text-align: right">
+                        <?php echo "$".$cart_item['price'];?>
+                        <br><?php echo $cart_item['quantity'];?>
+                        <br><?php echo $cart_item['batch_date'];?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php 
+}
+echo "<p align=right class='p-3'>Order Total: <strong>$".$total."</strong></p>";
+?>
+</div>
 
 <div class="row" align="center">
     <!--Go back to shopping button-->
     <div class="col">
-        <div class="form-group">
-            <form action="index.php">
-                <input type="submit" class="btn btn-outline-dark w-75 mx-auto" value="Return to Home">
-            </form>
-        </div>
+        <form action="index.php">
+            <input type="submit" class="btn btn-outline-dark w-75 mx-auto" value="Return to Home">
+        </form>
     </div>
     <!--View Orders-->
     <div class="col" >
-        <div class="form-group">
-            <form>
-                <input type="submit" class="btn btn-outline-dark w-75 mx-auto" value="View My Orders">
-            </form>
-        </div>
+
+        <form>
+            <input type="submit" class="btn btn-outline-dark w-75 mx-auto" value="View My Orders">
+        </form>
     </div>
 </div>
 
-
-
-
-  
-   </body>
+</div>
+</body>
 </html>
